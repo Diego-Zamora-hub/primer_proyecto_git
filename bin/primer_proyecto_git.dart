c3cf885c904bd34265126046d1ec739d726a81d9
Diego Zamora <zamoragallegodiegoalejandro@gmail.com>
2026-06-09 06:55:44 -0500

    Primera version

---
+++ bin/primer_proyecto_git.dart
@@ -0,0 +1,5 @@
+import 'package:primer_proyecto_git/primer_proyecto_git.dart' as primer_proyecto_git;
+
+void main(List<String> arguments) {
+  print('Hello world: ${primer_proyecto_git.calculate()}!');
+}
---
+++ test/primer_proyecto_git_test.dart
@@ -0,0 +1,8 @@
+import 'package:primer_proyecto_git/primer_proyecto_git.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

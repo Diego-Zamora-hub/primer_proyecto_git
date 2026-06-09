010594c99c7cb2fc098f11ac155f158de816fd56
Diego Zamora <zamoragallegodiegoalejandro@gmail.com>
2026-06-09 07:45:18 -0500

    Tercera version

--- bin/primer_proyecto_git.dart
+++ bin/primer_proyecto_git.dart
@@ -1,11 +1,11 @@
-void main(List<String> arguments) {
-    Map<String,String>contactos={
-      "juan":"303094049"
-      "zamora":"3938494"
-      "nicolas":"3443433"
-    };
-    print("lista de contactos:");
-    contactos.forEach((nombre,telefono){
-      print("$nombre:$telefono");
-    });
-    }
+import 'package:primer_proyecto_git/primer_proyecto_git.dart';
+
+void main() {
+  Map<String, String> contactos = {
+    "juan": "303094049",
+    "zamora": "3938494",
+    "nicolas": "3443433",
+  };
+
+  mostrar(contactos);
+}
